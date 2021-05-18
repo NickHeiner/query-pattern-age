c4605c0930b645cb5201bf88ebc9a87eb17cf158
Nick Heiner <nheiner@netflix.com>
2021-05-18 16:36:35 -0400

    Fix absolute paths in test snapshots

--- __tests__/index.js
+++ __tests__/index.js
@@ -6,17 +6,28 @@ const _ = require('lodash');
 const binPath = path.resolve(__dirname, '..', packageJson.bin['query-pattern-age']);
 const astArgs = ['--astSelector', 'CallExpression[callee.object.name=console][callee.property.name=log]'];
 const baseArgs = [
-  '--paths', path.resolve(__dirname, '..', '__fixtures__'), 
+  '--paths', path.join('..', '__fixtures__'), 
   ...astArgs
 ];
 
+/**
+ * @param {string[]} args 
+ * @param {import("execa").Options} opts
+ * @returns 
+ */
+// Setting "opts = {}" was the only way I could figure out to make opts optional.
+const execTest = (args, opts = {}) => execa(binPath, args, {
+  cwd: __dirname,
+  ...opts
+})
+
 describe('query-pattern-age', () => {
   it('raw', async () => {
-    const {stdout} = await execa(binPath, [...baseArgs, '--format', 'raw']);
+    const {stdout} = await execTest([...baseArgs, '--format', 'raw']);
     expect(JSON.parse(stdout)).toMatchSnapshot();
   });
   it('pretty', async () => {
-    const {stdout} = await execa(binPath, [...baseArgs, '--format', 'pretty'], {
+    const {stdout} = await execTest([...baseArgs, '--format', 'pretty'], {
       // If CI=true, the output will have no color. We want to assert that the output does have color.
       env: _.omit(process.env, 'CI'),
       extendEnv: false
@@ -24,10 +35,10 @@ describe('query-pattern-age', () => {
     expect(stdout).toMatchSnapshot();
   });
   it('only one file', async () => {
-    const {stdout} = await execa(binPath, [
+    const {stdout} = await execTest([
       ...astArgs, 
       '--format', 'raw', 
-      '--paths', path.resolve(__dirname, '..', '__fixtures__', 'also-contains-pattern.js')
+      '--paths', path.join('..', '__fixtures__', 'also-contains-pattern.js')
     ]);
     expect(JSON.parse(stdout)).toMatchSnapshot();
   });
